501083c8492e59a003d8375051467b9328e2e5fb
Shawn Chambless <shawn.chambless@gmail.com>
2015-06-09 10:07:59 -0600

    Added Week 4 Day 2 toy problem

--- script.js
+++ script.js
@@ -238,9 +238,9 @@ var nonRepeat = function(str) {
 nonRepeat('AABCABD')
 
 /*
-Write a funciton called ABCheck that takes a string parameter and returns the string true if the characters a and b are separated by exactly 3 places
-anywhere in the string and at least once (ie. 'lane borrowed' would result in true because there is exactly three characters between a and b).
-Otherwise return the string false
+    Write a funciton called ABCheck that takes a string parameter and returns the string true if the characters a and b are separated by exactly 3 places
+    anywhere in the string and at least once (ie. 'lane borrowed' would result in true because there is exactly three characters between a and b).
+    Otherwise return the string false
 */
 var abcheck = function(str) {
         for (var i = 0; i < str.length; i++) {
@@ -251,3 +251,21 @@ var abcheck = function(str) {
         return false;
 };
 abcheck('lane borrowed');
+/*
+    Week 4 Day 2 Toy Problem
+    Write a funciton that generates an array of integers of the Fibonacci sequence, up to i = 100. The Fibonacci sequence is defined by : Fn = Fn-1 + Fn-2, which means to find Fn you add the previous two numbers up. Teh first two numbers in the sequence are 0 and 1. Write a funciton to check if a given number is in the Fibonacci sequence, return 'yes' if it is, if not return the string 'no'.
+*/
+var fib = function(num) {
+    var fibArr = [];
+    fibArr[0] = 0;
+    fibArr[1] = 1;
+    for (var i = 2; i < 101; i++){
+        fibArr[i] = fibArr[i -1] + fibArr[i - 2];
+        console.log(fibArr);
+    }
+    if(fibArr.indexOf(num) !== -1) {
+        return 'yes';
+    }
+        return 'no';
+}
+fib(13);
